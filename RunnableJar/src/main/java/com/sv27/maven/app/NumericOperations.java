9941561261f1ef2a887e2593ac08ded1349eac93
Sivaraj Velayutham <>
2019-04-07 22:42:50 -0400

    Javadoc integration  - Maven Plugin.

--- RunnableJar/src/main/java/com/sv27/maven/app/NumericOperations.java
+++ RunnableJar/src/main/java/com/sv27/maven/app/NumericOperations.java
@@ -1,9 +1,21 @@
+/* License information -  GPU V2.0 */
 package com.sv27.maven.app;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
+/**
+ * This class is used to calculate the Statistics of an inout array.
+ * It uses Apache common-math lib.
+ * 
+ * @author SV27
+ *
+ */
 public class NumericOperations {
 	
+	/**
+	 * This method calculates the statistics of an input array.
+	 * @param inputArray {@link Double} array.
+	 */
 	static void findStat(double[] inputArray) {
 		
 		DescriptiveStatistics stat = new DescriptiveStatistics();
@@ -18,6 +30,10 @@ public class NumericOperations {
 		
 	}
 	
+	/**
+	 * This is the main method of our application.
+	 * @param args {@link String} Input arguments. 
+	 */
 	public static void main(String[] args) {
 		
 		double[] ipArray = {1.6, 2.8, 3.4, 0.2, 9.4, 2.3 };
